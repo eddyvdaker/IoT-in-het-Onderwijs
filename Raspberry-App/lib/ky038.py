9e4d99fa4319619e9dcc46ec87387883b400f040
Guy Dols <guydols@gmail.com>
2018-01-21 15:10:22 +0100

    Sound sensor callback created
    Able to get a list of sound events to get an average of noise.

--- Raspberry-App/lib/ky038.py
+++ Raspberry-App/lib/ky038.py
@@ -3,13 +3,26 @@ import time
 
 class ky038():
 	"""docstring for ky038"""
+	history = []
+
 	def __init__(self, pin):
 		self.pin = pin
 		GPIO.setmode(GPIO.BCM)
 		GPIO.setup(self.pin, GPIO.IN)
 
+	def listen(self,sec):
+		initial_time = time.time()
+		self.create_callback()
+		time.sleep(60)
+		print(self.history)
+		return self.history
+
+	def create_callback(self):
+		GPIO.add_event_detect(self.pin, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+		GPIO.add_event_callback(self.pin, self.callback)  # assign function to GPIO PIN, Run function on change
+
 	def read(self):
 		return GPIO.input(self.pin)
 
-# GPIO.add_event_detect(sound, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-# GPIO.add_event_callback(sound, callback)  # assign function to GPIO PIN, Run function on change
+	def callback(self,val):
+		self.history.append(1)
