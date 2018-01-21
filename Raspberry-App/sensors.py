eb820c07fb11248b5ee3412b68fd69f47d278c02
Guy Dols <guydols@gmail.com>
2018-01-21 15:12:02 +0100

    Testing purposes
    For now

--- Raspberry-App/sensors.py
+++ Raspberry-App/sensors.py
@@ -1,35 +1,47 @@
 import RPi.GPIO as GPIO
 import lib.dht11 as dht11
 import lib.ky038 as ky038
+import time
 
 
 class sensors():
 	"""Sensors"""
 	def __init__(self):
-		self.dht11Instance = dht11.DHT11(pin = 4)
-		self.ky038Instance = ky038(pin = 24)
-		self.refresh()
+		self.dht11Instance = dht11.DHT11(pin=4)
+		self.ky038Instance = ky038.ky038(pin=24)
 
 	def refresh(self):
-		self.ky038Result = ky038Instance.read()
-		self.dht11Result = dht11Instance.read()
+		self.ky038Result = self.ky038Instance.read()
+		self.dht11Result = self.dht11Instance.read()
 
 	def getTemp(self):
-		if dht11Result.is_valid():
+		if self.dht11Result.is_valid():
 		# temperature in degrees celcius
 			return self.dht11Result.temperature
 		else:
 			# return error if read failed
-			return dht11Result.error_code
+			return self.dht11Result.error_code
 
 	def getHum(self):
-		if dht11Result.is_valid():
+		if self.dht11Result.is_valid():
 			# humidity in percentage
 			return self.dht11Result.humidity
 		else:
 			# return error if read failed
-			return dht11Result.error_code
+			return self.dht11Result.error_code
 
 	def getSound(self):
 		# todo get avaerage loudness over time
 		return self.ky038Result
+
+	def loop(self):
+		self.ky038Instance.listen(60)
+		while True:
+			# self.refresh()
+			# print(self.getTemp())
+			# print(self.getHum())
+			# print(self.getSound())
+			time.sleep(1)
+
+s = sensors()
+s.loop()
