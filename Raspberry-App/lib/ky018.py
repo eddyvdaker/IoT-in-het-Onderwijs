e83d13588f86f1f2e3480854e98378616674a184
Guy Dols <guydols@gmail.com>
2018-01-23 14:06:51 +0100

    Fixed photoresistor

--- Raspberry-App/lib/ky018.py
+++ Raspberry-App/lib/ky018.py
@@ -1,19 +1,21 @@
-import RPi.GPIO as GPIO, time, os
+import RPi.GPIO as GPIO
+import time
+import os
 
-pin = 23
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(pin, GPIO.OUT)
-GPIO.output(pin, GPIO.LOW)
-time.sleep(0.1)
 
-def RCtime():
-	reading = 0
-	GPIO.setup(pin, GPIO.IN)
-	# This takes about 1 millisecond per loop cycle
-	while (GPIO.input(pin) == GPIO.LOW):
-		reading += 1
-	return reading
+class ky018:
+	"""docstring for ky018."""
 
-while True:
-	print(RCtime())     # Read RC timing using pin #18
+	def __init__(self, pin):
+		self.pin = pin
+
+	def read(self):
+		self.count = 0
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(self.pin, GPIO.OUT)
+		GPIO.output(self.pin, GPIO.LOW)
+		time.sleep(0.1)
+		GPIO.setup(self.pin, GPIO.IN)
+		while (GPIO.input(self.pin) == GPIO.LOW):
+			self.count += 1
+		return self.count
--- Raspberry-App/sensors.py
+++ Raspberry-App/sensors.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import lib.dht11 as dht11
 import lib.ky038 as ky038
+import lib.ky018 as ky018
 import time
 
 
@@ -9,10 +10,12 @@ class sensors():
 	def __init__(self):
 		self.dht11Instance = dht11.DHT11(pin=4)
 		self.ky038Instance = ky038.ky038(pin=24)
+		self.ky018Instance = ky018.ky018(pin=3)
 
 	def refresh(self):
 		self.ky038Result = self.ky038Instance.read()
 		self.dht11Result = self.dht11Instance.read()
+		self.ky018Result = self.ky018Instance.read()
 
 	def getTemp(self):
 		if self.dht11Result.is_valid():
@@ -32,15 +35,21 @@ class sensors():
 
 	def getSound(self):
 		# todo get avaerage loudness over time
-		return self.ky038Result
+		# self.ky038Instance.listen(60)
+		# return self.ky038Result
+		pass
+
+	def getLight(self):
+		# todo get avaerage loudness over time
+		return self.ky018Result
 
 	def loop(self):
-		self.ky038Instance.listen(60)
 		while True:
-			# self.refresh()
-			# print(self.getTemp())
-			# print(self.getHum())
+			self.refresh()
+			print(self.getTemp())
+			print(self.getHum())
 			# print(self.getSound())
+			print(self.getLight())
 			time.sleep(1)
 
 s = sensors()
