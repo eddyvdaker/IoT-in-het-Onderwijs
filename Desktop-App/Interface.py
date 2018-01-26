ca3ebc7c422b4d7a57d9cfd10cbc3f1f6aab76ab
Eddy van den Aker <eddy.v.d.aker@gmail.com>
2018-01-26 11:56:41 +0100

    Started switch to API instead of manual start

--- Desktop-App/Interface.py
+++ Desktop-App/Interface.py
@@ -4,8 +4,14 @@ This modules contains all code used to render the GUI and make it functional.
 from Trackers.KeyboardLogger import KeyboardLogger
 from Trackers.WindowLogger import WindowLogger
 from tkinter import *
-from time import strftime
+from time import strftime, sleep
+from threading import Thread
+from urllib.request import urlopen
 import json
+
+STUDENT_ID = 1
+IP = 'ts.guydols.nl'
+URL = f'http://{IP}:5000/'
 
 
 class ScrollableFrame(Frame):
@@ -41,8 +47,8 @@ class App:
     def __init__(self):
         # Create class variables
         self.session_running = False
-        self.session_not_running_text = 'Start Session'
-        self.session_running_text = 'Stop Session'
+        self.session_not_running_text = 'No session running'
+        self.session_running_text = 'Session running'
         self.study_activities = self.get_study_activities()
 
         # Create list of logger objects
@@ -63,18 +69,11 @@ class App:
         self.bottom_frame.pack()
 
         # Setup Top frame
-        self.start_btn = Button(self.top_frame,
-                                text=self.session_not_running_text,
-                                command=self.toggle_session, width=15)
-        self.start_btn.grid(row=0, sticky='w', padx=5, pady=10)
+        self.status_label = Label(self.top_frame,
+                                  text=self.session_not_running_text,
+                                  width=15)
 
-        self.study_activity = StringVar(self.top_frame)
-        self.study_activity.set(self.study_activities[0])
-        self.study_activity_menu = OptionMenu(self.top_frame,
-                                              self.study_activity,
-                                              *self.study_activities)
-        self.study_activity_menu.config(width=35)
-        self.study_activity_menu.grid(row=0, column=1, pady=10)
+        self.status_label.grid(row=0, sticky='w', padx=5, pady=10)
 
         # Bottom frame
         for i, logger in enumerate(self.loggers):
@@ -87,50 +86,55 @@ class App:
             checkbox.grid(row=i, sticky='w')
             self.loggers_to_run.append(var)
 
-    # Start running the GUI
-    def start(self):
-        self.root.mainloop()
+            self.check_thread = Thread(self.check_for_session())
 
-    # Toggle tracking session and change the button text
-    def toggle_session(self):
-        self.session_running = not self.session_running
-        if self.session_running:
-            self.start_btn.config(text=self.session_running_text)
-            self.start_loggers()
-        else:
-            self.start_btn.config(text=self.session_not_running_text)
-            self.stop_loggers()
+# Start running the GUI
+def start(self):
+    self.root.mainloop()
 
-    # Get study activities for the student from the smartphone app
-    # (not implemented yet)
-    def get_study_activities(self):
-        return ['Read chapter 2', 'Create design doc',
-                'Prepare for presentation']
 
-    # Starts each of the loggers that are turned on
-    def start_loggers(self):
-        self.loggers_to_run_at_start = self.loggers_to_run
-        for i, logger in enumerate(self.loggers):
-            if self.loggers_to_run[i].get() == 1:
-                logger.start_logging()
+# Checks with the API if a session has started for this student
+def check_for_session(self):
+    while True:
+        status = urlopen(f'{URL}?id={STUDENT_ID}')
+        status = status.read().decode('utf-8')
+        print(status)
+        sleep(1)
 
-    # Stops each of the loggers, starts log collection and log writing
-    def stop_loggers(self):
-        for logger in self.loggers:
-            logger.stop_logging()
-        logs = self.get_logs()
-        self.store_logs(logs)
+# Toggle tracking session and change the button text
+def toggle_session(self):
+    self.session_running = not self.session_running
+    if self.session_running:
+        self.start_btn.config(text=self.session_running_text)
+        self.start_loggers()
+    else:
+        self.start_btn.config(text=self.session_not_running_text)
+        self.stop_loggers()
 
-    # Collects the logs from each of the loggers
-    def get_logs(self):
-        logs = {}
-        for i, logger in enumerate(self.loggers):
-            if self.loggers_to_run_at_start[i].get() == 1:
-                logs.update({logger.get_info()['name']: logger.get_log()})
-        return logs
+# Starts each of the loggers that are turned on
+def start_loggers(self):
+    self.loggers_to_run_at_start = self.loggers_to_run
+    for i, logger in enumerate(self.loggers):
+        if self.loggers_to_run[i].get() == 1:
+            logger.start_logging()
 
-    # Writes the logs to a results file
-    def store_logs(self, logs):
-        output_file = f'results{strftime("_%Y-%m-%d_%H-%M")}.json'
-        with open(output_file, 'w') as file:
-            json.dump(logs, file, sort_keys=True, indent=4)
+# Stops each of the loggers, starts log collection and log writing
+def stop_loggers(self):
+    for logger in self.loggers:
+        logger.stop_logging()
+    logs = self.get_logs()
+    self.store_logs(logs)
+
+# Collects the logs from each of the loggers
+def get_logs(self):
+    logs = {}
+    for i, logger in enumerate(self.loggers):
+        if self.loggers_to_run_at_start[i].get() == 1:
+            logs.update({logger.get_info()['name']: logger.get_log()})
+    return logs
+
+# Writes the logs to a results file
+def store_logs(self, logs):
+    output_file = f'results{strftime("_%Y-%m-%d_%H-%M")}.json'
+    with open(output_file, 'w') as file:
+        json.dump(logs, file, sort_keys=True, indent=4)
