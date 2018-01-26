9c1e63a7223b497a5daca18c56cb16d524d0ef3e
Eddy van den Aker <eddy.v.d.aker@gmail.com>
2018-01-26 11:57:28 +0100

    Basic dashboard

---
+++ Server/dashboard.py
@@ -0,0 +1,10 @@
+"""
+Contains any additional code needed to create the StudyBuddy Dashboard
+"""
+
+
+def generate_page_vars():
+    page_vars = {
+        'user_type': user_type
+    }
+    return page_vars
