281fd956528b12f2fbb76a1c5874c536901449ff
Guy Dols <guydols@gmail.com>
2018-01-27 21:08:22 +0100

    Major functionality working
    Multithreading, communications, interactions

--- Raspberry-App/__init__.py
+++ Raspberry-App/__init__.py
@@ -8,14 +8,19 @@ class controller:
 	"""docstring for ."""
 	def __init__(self):
 		self.recording = False
+		self.id = None
 
-		self.sensors_queue = Queue()
-		self.sensors_thread = Thread(target=sensors,args=(self.sensors_queue))
+		self.sensors_in = Queue()
+		self.sensors_out = Queue()
+		self.sensors_thread = Thread(target=sensors,
+		args=(self.sensors_in, self.sensors_out, ))
 		self.sensors_thread.daemon = True
 		self.sensors_thread.start()
 
-		self.comms_queue = Queue()
-		self.comms_thread = Thread(target=comms,args=(self.comms_queue))
+		self.comms_in = Queue()
+		self.comms_out = Queue()
+		self.comms_thread = Thread(target=comms,
+		args=(self.comms_in, self.comms_out, ))
 		self.comms_thread.daemon = True
 		self.comms_thread.start()
 
@@ -23,18 +28,37 @@ class controller:
 
 	def loop(self):
 		while True:
-			cur = comms_queue.get()
-			if cur[] == "x":
+			cur = self.comms_in.get()
+			if cur["status"] == "running" and self.recording == False:
+				print("starting sensors, __init__.py")
 				self.recording = True
-			elif cur[] == "y":
+				self.id = cur["id"]
+				self.startRec()
+			elif cur["status"] == "stopped" and self.recording == True:
+				print("stopping sensors, __init__.py")
 				self.recording = False
-			time.sleep(60)
+				self.stopRec()
+				self.id = None
+			else:
+				print("Nothing do to...")
+			time.sleep(5)
 
 	def startRec(self):
-		pass
+		self.sensors_in.put("start")
 
 	def stopRec(self):
-		pass
+		self.sensors_in.put("stop")
+		data = self.sensors_out.get()
+		temps=[]
+		humis=[]
+		for dht in data[0]:
+			temps.append(dht[0][0])
+			humis.append(dht[0][1])
+		self.comms_out.put(["temperatuur",temps,self.id])
+		self.comms_out.put(["vochtigheid",humis,self.id])
+		self.comms_out.put(["geluid",data[1],self.id])
+		self.comms_out.put(["light",data[2],self.id])
+		self.id = None
 
 if __name__ == '__main__':
 	instance = controller()
--- Raspberry-App/communication.py
+++ Raspberry-App/communication.py
@@ -1,35 +1,46 @@
-import httplib
+import http.client
 import json
+import time
+from queue import Empty
 
-class comm:
+class communication:
 	"""docstring for comm."""
 	def __init__(self):
-		self.conn = httplib.HTTPConnection("ts.guydols.nl:5000")
+		self.conn = http.client.HTTPConnection("ts.guydols.nl:5000")
 
 	def getCommand(self):
-		self.conn.request("GET","/check_sessions?id=1")
+		self.conn.request("GET","/check_session?id=1")
 		res = self.conn.getresponse()
-		if res.status == "200":
+		if res.status == 200:
 			data = json.loads(res.read())
 			return data
 		else:
 			return None
 
-	def postData(self):
-		self.conn.request("GET","/blabla")
+	def postData(self,data):
+		headers = {'Content-type': 'application/json'}
+		self.conn.request("POST","/upload_data",data,headers)
 		res = self.conn.getresponse()
-		if res.status == "200":
-			data = json.load(res.read())
-			return data
+		if res.status == 200:
+			return True
 		else:
-			return None
+			return False
 
-def main(comms_queue):
-	comm = comm()
+def main(comms_in, comms_out):
+	comm = communication()
 	while True:
 		cmd = comm.getCommand()
 		if cmd == None:
 			pass
 		else:
-			comms_queue.put(cmd)
-		time.sleep(60)
+			comms_in.put(cmd)
+		try:
+			data = comms_out.get_nowait()
+			json_data = {
+			"data_type": data[0],
+			"sessiondata": data[1],
+			"sessionid":data[2]}
+			comm.postData(json.dumps(json_data))
+		except Empty:
+			pass
+		time.sleep(5)
